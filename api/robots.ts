b1608b98481afc6ed70afc379e975abde7d58558
ahmedM51 <ahmedmohamed4336@gmail.com>
2026-02-28 09:49:49 +0200

    Fix formatting of sitemap in robots.ts

--- api/robots.ts
+++ api/robots.ts
@@ -2,8 +2,7 @@ export default async function handler(req: any, res: any) {
   const txt = `User-agent: *
 Allow: /
 
-Sitemap: https://smart-student-platform-21.vercel.app/sitemap.xml
-`;
+Sitemap: https://smart-student-platform-21.vercel.app/sitemap.xml`;
 
   res.setHeader('Content-Type', 'text/plain; charset=utf-8');
   res.setHeader('Cache-Control', 'public, max-age=0, s-maxage=86400');
